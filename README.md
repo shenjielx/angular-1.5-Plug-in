798a03ffdfe90779d61f625f5464e2194b990a35
Daxia <shenjielx@live.com>
2017-01-22 13:41:55 +0800

    fix dateTimeToLocal

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # angular-1.5-Plug-in
 Angular 1.5 自定义的一些插件
 
+### 2017-01-22 lists:
+1.fix dateTimeToLocalString filter.（修复时间显示本地化的指令）
+
 ### 2016-10-28 lists:
 1.update dateTimeToLocalString filter.（更新时间显示本地化的指令）
 
--- filter.dateTimeToLocalString.js
+++ filter.dateTimeToLocalString.js
@@ -1,23 +1,31 @@
 appModule.filter('dateTimeToLocalString', ['$filter', function($filter) {
     return function(data, key) {
-        if (!data) return data;
-        if (!key) key = 0;
+		if (!data) return data;
+		if (!key) key = 0;
+		var format;
+		if (key === 1) {
+			format = "yyyy-MM-dd";
+		} else if (key === 2) {
+			format = "HH:mm:ss";
+		} else if (key === 3) {
+			format = "MM-dd HH:mm:ss";
+		} else {
+			format = "yyyy-MM-dd HH:mm:ss";
+		}
+		if (data.indexOf('T') > -1)
+			data = data.replace('T', ' ').split('.')[0];
 
-        var format;
-        if (key == 1) {
-            format = "yyyy-MM-dd";
-        } else if (key == 2) {
-            format = "HH:mm:ss";
-        } else if (key == 3) {
-            format = "MM-dd HH:mm:ss";
-        } else {
-            format = "yyyy-MM-dd HH:mm:ss";
-        }
-        if (data.indexOf('Z') === -1 && data.indexOf('+') === -1) {
-            data += 'Z';
-        }
-        var datestamp = Date.parse(data);
-        return $filter('date')(datestamp, format);
+		if (window.ActiveXObject || "ActiveXObject" in window)
+			data = data.replace('-', '/').replace('-', '/');
+		else
+			data = data.replace('/', '-').replace('/', '-');
+
+		if (data.indexOf('Z') === -1 && data.indexOf('+') === -1) {
+			data += 'Z';
+		}
+		var datestamp = Date.parse(data);
+		if (datestamp) return $filter('date')(datestamp, format);
+		else return '';
     };
 }]);
 
